62943b2cb46f74ef2b9f839ca760a0aaad8ea874
Yash tiwari <123789297+Y-tiwari@users.noreply.github.com>
2023-03-04 10:24:17 +0530

    Add files via upload

---
+++ p.js
@@ -0,0 +1,8 @@
+function login() {
+    if ($("#username").val() == "yash" && $("#password").val() == "1234") {
+      alert("You are a valid user");
+    } else {
+      alert("You are not a valid user");
+    }
+  }
+  
